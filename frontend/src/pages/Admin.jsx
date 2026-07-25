5454e96d1264993ab9e04b622278aca089231e89
Godwin Augustine <godwinaugustine118@gmail.com>
2026-07-25 01:00:50 +0100

    Add StatCard component to Admin page

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 
 import Header from "../components/layout/Header";
-
+import StatCard from "../components/admin/StatCard";
 export default function Admin() {
   
   return (
@@ -13,6 +13,15 @@ export default function Admin() {
 
     <div className="pt-36 px-3 h-[calc(100vh-70px)] overflow-y-auto scrollbar-hide">
        Admin
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+  <StatCard
+    title="Total Users"
+    value={0}
+    icon={<FaUsers />}
+    active={false}
+    onClick={() => {}}
+  />
+</div>
     </div>
   </div>
   );
